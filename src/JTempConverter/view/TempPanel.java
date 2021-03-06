ffe74f246836752cd20676d422ba9d5bb592c4e1
Dash11235 <61665703+Dash11235@users.noreply.github.com>
2021-03-06 17:45:04 +0100

    Update TempPanel.java

--- src/JTempConverter/view/TempPanel.java
+++ src/JTempConverter/view/TempPanel.java
@@ -2,8 +2,11 @@ package JTempConverter.view;
 
 import javax.swing.*;
 import java.awt.*;
+import JTempConverter.controller.TempController;
 
 public class TempPanel extends JPanel {
+    private TempController control;
+
     private JButton bConvert;
     private JButton bReset;
 
@@ -15,8 +18,63 @@ public class TempPanel extends JPanel {
     private JPanel pOpane;
 
     private JPanel buttonPane;
-    public TempPanel() {
-        setLayout(new BorderLayout());
+    public TempPanel(TempController control){
+        this.control = control;
+        this.setLayout(new BorderLayout());
 
+        /*
+        ---------------------------------------------------------------
+                               OBJECT FACTORY
+        ---------------------------------------------------------------
+         */
+
+        String[] combos = { "Celsius -> Fahrenheit",
+                "Fahrenheit -> Celsius",
+                "Celsius -> Kelvin",
+                "Kelvin -> Celsius",
+                "Fahrenheit -> Kelvin",
+                "Kelvin -> Fahrenheit" };
+
+        pIpane = new JPanel();
+        pOpane = new JPanel();
+
+        lOutput = new JLabel("output");
+        tInput = new JTextField();
+
+        bConvert = new JButton("Convert");
+        bReset = new JButton("Reset");
+
+
+        dConversionMenu = new JComboBox(combos);
+
+        /*
+        ---------------------------------------------------------------
+                               MODIFYING
+        ---------------------------------------------------------------
+         */
+
+        // VOID 
+
+        /*
+        ---------------------------------------------------------------
+                               ADDING
+        ---------------------------------------------------------------
+         */
+
+        pIpane.add(dConversionMenu);
+        pIpane.add(tInput);
+
+        pOpane.add(lOutput);
+        pOpane.add(bConvert);
+        pOpane.add(bReset);
+
+        this.add(pIpane, BorderLayout.NORTH);
+        this.add(lOutput, BorderLayout.SOUTH);
     }
+
+    public String getInput() { return this.tInput.getText(); }
+
+    public String getCombo() { return this.dConversionMenu.getActionCommand(); }
+
+    public void setLabelText(String str) { this.lOutput.setText(str); }
 }
