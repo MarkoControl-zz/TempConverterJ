4b62f7f337907bea4edd47cd020a44a3decbb69b
Dash11235 <61665703+Dash11235@users.noreply.github.com>
2021-03-06 23:12:27 +0100

    Update TempController.java

--- src/JTempConverter/controller/TempController.java
+++ src/JTempConverter/controller/TempController.java
@@ -1,17 +1,27 @@
 package JTempConverter.controller;
 
 import JTempConverter.view.*;
+import JTempConverter.model.*;
 
-public class TempController {
-    public static void main (String [] args) {
-        new TempController();
-    }
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    private TempFrame tf;
+public class TempController implements ActionListener {
+
     private TempPanel tp;
+    private TempFrame tf;
+    private Model model;
 
-    public TempController() {
+    public static void main(String[] args) { new TempController(); }
+
+    public TempController(){
         tp = new TempPanel(this);
         tf = new TempFrame(tp);
+        model = new Model();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
