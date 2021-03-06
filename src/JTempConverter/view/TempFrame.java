b03f80e8dbaf1fb63de249d77a2c70e913814dba
MarkoControl <markocontroll@gmail.com>
2021-03-06 16:13:46 +0100

    Finished frame, panel components as attributes

--- src/JTempConverter/view/TempFrame.java
+++ src/JTempConverter/view/TempFrame.java
@@ -1,2 +1,15 @@
-package JTempConverter.view;public class TempFrame {
+package JTempConverter.view;
+
+import javax.swing.*;
+
+public class TempFrame extends JFrame {
+    public TempFrame(TempPanel tp) {
+        super("Temperature Converter");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setSize(500, 300);
+        add(tp);
+        setVisible(true);
+    }
+
 }
--- src/JTempConverter/view/TempPanel.java
+++ src/JTempConverter/view/TempPanel.java
@@ -1,2 +1,22 @@
-package JTempConverter.view;public class TempPanel {
+package JTempConverter.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class TempPanel extends JPanel {
+    private JButton bConvert;
+    private JButton bReset;
+
+    private JComboBox dConversionMenu;
+    private JTextField tInput;
+    private JLabel lOutput;
+
+    private JPanel pIpane;
+    private JPanel pOpane;
+
+    private JPanel buttonPane;
+    public TempPanel() {
+        setLayout(new BorderLayout());
+
+    }
 }
