8f2f80403c9a7fe6aea199586ece5f6205e5483a
Dash11235 <61665703+Dash11235@users.noreply.github.com>
2021-03-07 15:08:46 +0100

    Update TempModel.java

--- src/JTempConverter/model/TempModel.java
+++ src/JTempConverter/model/TempModel.java
@@ -1,16 +1,33 @@
+
 package JTempConverter.model;
 
 public class TempModel {
 
-        private double temperature;
+    private double temperature;
 
-        public void celsiusToFahrenheit(double celsius) {
-            temperature = (celsius * 9.0 / 5.0) +32.0;
-        }
+    public void celsiusToFahrenheit(double celsius) {
+        temperature = (celsius * 9.0 / 5.0) +32.0;
+    }
 
-        public void fahrenheitToCelsius(double fahrenheit) {
-            temperature = (fahrenheit - 32.0) * 5.0 /9.0;
-        }
+    public void fahrenheitToCelsius(double fahrenheit) {
+        temperature = (fahrenheit - 32.0) * 5.0 /9.0;
+    }
 
-        public double getTemperature(){return this.temperature;}
+    public void celsiusToKelvin(double celsius) {
+        temperature = celsius + 273.15;
+    }
+
+    public void kelvinToCelsius(double kelvin) {
+        temperature = kelvin - 273.15;
+    }
+
+    public void fahrenheitToKelvin(double fahrenheit) {
+        temperature = (fahrenheit - 32.0) * 5.0/9.0 + 273.15;
+    }
+
+    public void kelvinToFahrenheit(double kelvin) {
+        temperature = (kelvin - 273.15) * 9.0 / 5.0 + 32;
+    }
+
+    public double getTemperature(){return this.temperature;}
 }
