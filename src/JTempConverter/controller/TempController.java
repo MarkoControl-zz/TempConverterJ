607f5b901d5b21c9998c69a5380f486bd80de605
MarkoControl <markocontroll@gmail.com>
2021-03-07 02:47:04 +0100

    created celsiusToFahrenheit and fahrenheitToCelsius in model, and added functionality in the controller;

--- src/JTempConverter/controller/TempController.java
+++ src/JTempConverter/controller/TempController.java
@@ -10,18 +10,31 @@ public class TempController implements ActionListener {
 
     private TempPanel tp;
     private TempFrame tf;
-    private Model model;
+    private TempModel tm;
 
     public static void main(String[] args) { new TempController(); }
 
     public TempController(){
         tp = new TempPanel(this);
         tf = new TempFrame(tp);
-        model = new Model();
+        tm = new TempModel();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        Object o = e.getActionCommand();
+        if ((o.equals("Convert")) && (tp.getCombo().equals("Celsius -> Fahrenheit"))) {
+            double temp = Double.parseDouble(tp.getInput());
+            tm.celsiusToFahrenheit(temp);
+            String txt = "" + tm.getTemperature();
+            tp.setLabelText(txt);
+        }
 
+        if ((o.equals("Convert")) && (tp.getCombo().equals("Fahrenheit -> Celsius"))) {
+            double temp = Double.parseDouble(tp.getInput());
+            tm.fahrenheitToCelsius(temp);
+            String txt = "" + tm.getTemperature();
+            tp.setLabelText(txt);
+        }
     }
 }
--- src/JTempConverter/model/TempModel.java
+++ src/JTempConverter/model/TempModel.java
@@ -1,2 +1,16 @@
-package JTempConverter.model;public class TempModel {
+package JTempConverter.model;
+
+public class TempModel {
+
+        private double temperature;
+
+        public void celsiusToFahrenheit(double celsius) {
+            temperature = (celsius * 9.0 / 5.0) +32.0;
+        }
+
+        public void fahrenheitToCelsius(double fahrenheit) {
+            temperature = (fahrenheit - 32.0) * 5.0 /9.0;
+        }
+
+        public double getTemperature(){return this.temperature;}
 }
